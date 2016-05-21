c57f0fa0b11935ffe4a6528aa4c12701494ae44d
Makarios Christakis <makchr0@gmail.com>
2016-05-21 16:26:24 +0300

    Create reading-structures.c

---
+++ reading-structures.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct test{
+	int x;
+	struct test *nextptr;
+}Test;
+
+int main() {
+	int i;
+	FILE *input=fopen("output.bin","rb");
+	
+
+	
+	return 0;
+}
